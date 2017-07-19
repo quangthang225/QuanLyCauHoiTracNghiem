302181b3e558abcd8514936fde53261cf695dfbc
Tuan Tran <tltuan.hcmus@gmail.com>
2017-07-19 17:56:35 +0700

    Upload script setup Integrity Constraint

---
+++ Database/CaiDatRangBuoc.sql
@@ -0,0 +1,63 @@
+﻿USE [QuanLyCauHoiTracNghiem]
+GO  
+
+--1.Số đáp án của 1 câu hỏi là từ 2 tới 10 đáp án
+CREATE TRIGGER tg_SoLuongCauTraLoiCuaCauHoi ON CAUTRALOI
+AFTER INSERT, UPDATE, DELETE
+AS
+BEGIN
+	DECLARE @SoLuongCauTraLoi INTEGER
+	
+	IF NOT EXISTS( SELECT * FROM deleted)
+	BEGIN
+		--trường hợp insert
+		SELECT @SoLuongCauTraLoi = COUNT(C.MACH) FROM CAUTRALOI C,inserted I WHERE C.MACH = I.MACH 
+		IF ( @SoLuongCauTraLoi > 10 ) 
+		BEGIN
+			RAISERROR ('Số câu trả lời của 1 câu hỏi phải là từ 2 tới 10.', 16, 1);  
+			ROLLBACK TRANSACTION;  
+		END
+	END
+	ELSE
+	BEGIN
+		IF NOT EXISTS ( SELECT * FROM inserted )
+		BEGIN
+			--trường hợp delete
+			SELECT @SoLuongCauTraLoi = COUNT(C.MACH) FROM CAUTRALOI C,deleted I WHERE C.MACH = I.MACH 
+			IF ( @SoLuongCauTraLoi < 2 ) 
+			BEGIN
+				RAISERROR ('Số câu trả lời của 1 câu hỏi là từ 2 tới 10.', 16, 1);  
+				ROLLBACK TRANSACTION;  
+			END
+		END
+		ELSE
+		BEGIN
+			--trường hợp update
+			SELECT @SoLuongCauTraLoi = COUNT(C.MACH) FROM CAUTRALOI C,inserted I WHERE C.MACH = I.MACH 
+			IF ( @SoLuongCauTraLoi < 2 OR @SoLuongCauTraLoi > 10 ) 
+			BEGIN
+				RAISERROR ('Số câu trả lời của 1 câu hỏi là từ 2 tới 10.', 16, 1);  
+				ROLLBACK TRANSACTION;  
+			END
+		END
+	END
+END
+GO
+
+CREATE TRIGGER tg_SoCauTraLoiCuaCauHoi ON CAUHOI
+AFTER INSERT, UPDATE
+AS
+BEGIN
+	DECLARE @SoLuongCauTraLoi INTEGER
+	
+	SELECT @SoLuongCauTraLoi = C.SOCAUTRALOI FROM CAUHOI C,inserted I WHERE C.MACH = I.MACH 
+	IF ( @SoLuongCauTraLoi < 2 OR @SoLuongCauTraLoi > 10 ) 
+	BEGIN
+		RAISERROR ('Số câu trả lời của 1 câu hỏi phải là từ 2 tới 10.', 16, 1);  
+		ROLLBACK TRANSACTION;  
+	END
+	
+END
+GO
+
+--2.Điểm của một bộ đề thi tối đa là 10 
