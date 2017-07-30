abccb518d4d361b080e05caea5f92c13880032ee
TUAN-PC\TUAN <tltuan.hcmus@gmail.com>
2017-07-30 22:45:09 +0700

    Add script sp for QuanLyBoDeThi

---
+++ Database/TuanTran_spQuanLyBoDeThi/QuanLyBoDeThi.sql
@@ -0,0 +1,49 @@
+﻿CREATE PROCEDURE sp_TimKiemBoDeThi
+@MABDT bigint,
+@TenBDT nvarchar(255),
+@HocKy int,
+@NamHoc int
+AS
+BEGIN
+	BEGIN TRAN
+		BEGIN TRY
+			SELECT *
+			FROM BODETHI
+			WHERE (MABDT = @MABDT OR @MABDT = 0 ) 
+					OR ( TENBDT like N'%' + @TenBDT + '%' OR @TenBDT = '' )
+					OR ( HOCKY = @HocKy OR @HocKy = 0 )
+					OR ( NAMHOC = @NamHoc OR @NamHoc = 0 )
+			RETURN 1
+		END TRY
+		BEGIN CATCH
+			ROLLBACK TRAN
+			RETURN 0
+		END CATCH
+	COMMIT
+END
+GO
+
+CREATE PROCEDURE sp_TimKiemTatCaCauHoiTheoBoDeThi
+@MABDT bigint
+AS
+BEGIN
+	BEGIN TRAN
+		BEGIN TRY
+			--Kiểm tra mã bộ đề thi input có tồn tại hay không
+			IF NOT EXISTS ( SELECT * FROM BODETHI WHERE MABDT = @MABDT )
+			BEGIN
+				RETURN 0;
+			END 
+
+			SELECT *
+			FROM TAOBODETHI
+			WHERE MABDT = @MABDT
+			RETURN 1
+		END TRY
+		BEGIN CATCH
+			ROLLBACK TRAN
+			RETURN 0
+		END CATCH
+	COMMIT
+END
+GO
